a6d040bc6062992ef8fa000102237e588310f3ca
Sn3cor <jakub.krupa23@gmail.com>
2026-01-21 11:35:33 +0100

    finished frontend rewwork

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -8,11 +8,11 @@ const Wrapper = ({ children }: PageProps) => {
         <div className="wrapper">
             <div className="header">
                 <h1>Visualization for Finite Elements Method</h1>
-                <h2>Problem: material layers acoustic vibrations</h2>
+                <h2>Problem: heat transport equation</h2>
             </div>
             {children}
             <div className="footer">
-                <p>Jakub Krupa</p>
+                <p>Jakub Krupa 2026</p>
             </div>
         </div>
     )
